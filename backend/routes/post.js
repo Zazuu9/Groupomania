09fd8a2248688ff9e7aca520143c34ea014010ea
Kevin Gouaille <kevingouaille8515@gmail.com>
2022-08-07 23:10:13 +0200

    Middleware Auth

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
 const postCtrl = require('../controllers/post');
 const multer = require('../middleware/multer-config');
 
-router.get('/', postCtrl.getAllPost);
-router.get('/:id', postCtrl.getOnePost);
-router.post('/', multer, postCtrl.createPost);
-router.put('/:id', postCtrl.modifyPost);
-router.delete('/:id', postCtrl.deletePost);
+router.get('/', auth, postCtrl.getAllPost);
+router.get('/:id', auth, postCtrl.getOnePost);
+router.post('/', auth, multer, postCtrl.createPost);
+router.put('/:id', auth, postCtrl.modifyPost);
+router.delete('/:id', auth, postCtrl.deletePost);
 
 module.exports = router;
