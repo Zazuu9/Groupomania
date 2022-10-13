9a57c23ddf2a3372ee09b65be39f41a2802b8b7e
Kevin Gouaille <kevingouaille8515@gmail.com>
2022-10-13 19:49:50 +0200

    cookie 2

--- backend/app.js
+++ backend/app.js
@@ -6,23 +6,25 @@ const path = require("path");
 const app = express();
 const dotenv = require("dotenv").config();
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+const allowedOrigins = [
+    "http://localhost:3000",
+    "http://groupomania.kgouaille.fr",
+    "http://api.groupomania.kgouaille.fr",
+];
 
 mongoose
     .connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log("Connexion à MongoDB réussie !"))
     .catch(() => console.log("Connexion à MongoDB échouée !"));
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://groupomania.kgouaille.fr");
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-    );
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    next();
-});
-
+app.use(
+    cors({
+        credentials: true,
+        origin: allowedOrigins,
+    })
+);
 app.use(express.json());
 app.use(cookieParser());
 
